081dcaa8462a7997821563911225537ab32ad80f
Kartikeya Mishra <kmishra@umd.edu>
2022-03-28 17:35:22 -0400

    Added the Manhattan heuristic for faster traversal

--- node.py
+++ node.py
@@ -32,8 +32,9 @@ class Node:
         x, y, _ = self.coord
         x1, y1, _ = CONSTANT.GOAL_NODE
         
-        self.cost2go = round(math.sqrt((x1 - x)**2 + (y1 - y)**2))
-    
+        #self.cost2go = round(math.sqrt((x1 - x)**2 + (y1 - y)**2))
+        self.cost2go = abs(x1 - x) + abs(y1 - y)
+
     @staticmethod
     def actionCost():
         return CONSTANT.VECTOR_LEN
